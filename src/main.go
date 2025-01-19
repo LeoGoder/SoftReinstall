c274ec45dbbc5981df798ee4279e130b6dde5abd
LeoGoder <l.goderneaux@ik.me>
2025-01-19 17:01:41 +0100

    fix issue with policy

--- src/main.go
+++ src/main.go
@@ -7,48 +7,52 @@ import (
 )
 
 func main() {
-	// var for install process for chocolatey
-	var checkExecuPolicy = exec.Command("cmd", "/c", "powershell Get-ExecutionPolicy")
-	var checkExecuPolicyOut, checkExecuPolicyErr = checkExecuPolicy.CombinedOutput()
+	var i = true
+	for i {
+		// var for install process for chocolatey
+		var checkExecuPolicy = exec.Command("cmd", "/c", "powershell Get-ExecutionPolicy")
+		var checkExecuPolicyOut, checkExecuPolicyErr = checkExecuPolicy.CombinedOutput()
 
-	if checkExecuPolicyErr != nil {
-		fmt.Println(checkExecuPolicyErr.Error())
-	}
-
-	execuPolicy := strings.TrimSpace(string(checkExecuPolicyOut))
-
-	if execuPolicy != "restricted" {
-		fmt.Println("Execution Policy is not restricted")
-
-		var chocoinstall = exec.Command("cmd", "/c", "powershell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))")
-		var chocoinstallOut, chocoinstallErr = chocoinstall.CombinedOutput()
-
-		if chocoinstallErr != nil {
-			fmt.Println(chocoinstallErr.Error())
-		}
-		fmt.Println(string(chocoinstallOut))
-	} else {
-		fmt.Println("Execution Policy is restricted")
-
-		var setExecuPolicy = exec.Command("cmd", "/c", "powershell Set-ExecutionPolicy AllSigned")
-		var setExecuPolicyOut, setExecuPolicyErr = setExecuPolicy.CombinedOutput()
-
-		if setExecuPolicyErr != nil {
-			fmt.Println(setExecuPolicyErr.Error())
+		if checkExecuPolicyErr != nil {
+			fmt.Println(checkExecuPolicyErr.Error())
 		}
 
-		fmt.Println(string(setExecuPolicyOut))
+		execuPolicy := strings.TrimSpace(string(checkExecuPolicyOut))
 
+		if execuPolicy != "Restricted" {
+			fmt.Println("Execution Policy is not restricted")
+
+			var chocoinstall = exec.Command("cmd", "/c", "powershell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))")
+			var chocoinstallOut, chocoinstallErr = chocoinstall.CombinedOutput()
+
+			if chocoinstallErr != nil {
+				fmt.Println(chocoinstallErr.Error())
+			}
+			fmt.Println(string(chocoinstallOut))
+		} else {
+			fmt.Println("Execution Policy is restricted")
+
+			var setExecuPolicy = exec.Command("cmd", "/c", "powershell Set-ExecutionPolicy AllSigned")
+			var setExecuPolicyOut, setExecuPolicyErr = setExecuPolicy.CombinedOutput()
+
+			if setExecuPolicyErr != nil {
+				fmt.Println(setExecuPolicyErr.Error())
+			}
+
+			fmt.Println(string(setExecuPolicyOut))
+
+		}
+
+		// var for Install process with choco
+		var wincmd = exec.Command("cmd", "/c", "choco install firefox -y && choco install discord.install -y && choco install vscode -y && choco install steam -y && choco install epicgameslauncher -y && choco install obsidian -y && choco install 7zip -y && choco install git -y && -y && choco install python -y && choco install golang -y && choco install docker-desktop -y && choco install vlc -y")
+		var winout, winerr = wincmd.CombinedOutput()
+
+		if winerr != nil {
+			fmt.Println(winerr.Error())
+			return
+		}
+
+		fmt.Println(string(winout))
 	}
 
-	// var for Install process with choco
-	var wincmd = exec.Command("cmd", "/c", "choco install googlechrome -y")
-	var winout, winerr = wincmd.CombinedOutput()
-
-	if winerr != nil {
-		fmt.Println(winerr.Error())
-		return
-	}
-
-	fmt.Println(string(winout))
 }
